5814bb54eb1bf996d93dd8690871b622bcf4d7e0
Racha777 <jramoschang@gmail.com>
2022-08-24 21:05:51 -0500

    Arreglo de carpetas

--- src/routes/pedido.route.js
+++
@@ -1,9 +0,0 @@
-const {Router}=require('express');
-const router=Router();
-
-const {create}=require('../controllers/pedido.controllers');
-
-router.route('/')
-    .post(create)
-
-module.exports=router
